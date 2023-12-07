8a8c18e13a8e2789931e59c87660e1fad0901220
Jacob Benedict <66215501+JacobBndct@users.noreply.github.com>
2023-12-07 18:17:58 +0100

    Update media.js

--- routes/media.js
+++ routes/media.js
@@ -97,7 +97,7 @@ router.route('/add').post((req, res) => {
 //#region delete routes
 
 // delete media by id route
-router.route('/:id').delete((req, res) => {
+router.route('/delete/:id').delete((req, res) => {
     Media.findByIdAndDelete(req.params.id)
     .then(() => res.json('Media was deleted'))
     .catch(err => res.status(400).json('Error: ' + err));
