ccecfe69f074080facb6545a2eb1f664a3247103
Jacob Benedict <JacobBndct@gmail.com>
2025-04-27 15:04:45 -0400

    testing new build

--- client/src/components/mobius.component.js
+++ client/src/components/mobius.component.js
@@ -3,6 +3,16 @@ import React, { Component } from 'react';
 import '../CSS/gallery.css';
 
 export default class Mobius extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+    
+        const form = e.target;
+        const formData = new FormData(form);
+    
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
+      }
+
     render() {
         return (
             <div className='page-container'>
@@ -10,8 +20,10 @@ export default class Mobius extends Component {
                     <h3>Mobius</h3>
                     <div className='normal-section'>
                         <p>Note to self: don't forget to store code someplace safe!</p>
-                        <p>Note to note to self: I've put the code </p> 
-                        <input>Code</input>
+                        <p>Note to note to self: I've put the code </p>
+                        <label>
+                            Code: <input name='code'></input>
+                        </label>
                     </div>
                 </div>
             </div>
