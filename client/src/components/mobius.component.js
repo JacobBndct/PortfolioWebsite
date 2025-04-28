d3e0fb707c4284fa234e6c287a239fc25880c0ba
Jacob Benedict <JacobBndct@gmail.com>
2025-04-28 04:18:50 -0400

    Adjusted text

--- client/src/components/mobius.component.js
+++ client/src/components/mobius.component.js
@@ -53,10 +53,8 @@ export default class Mobius extends Component {
                             <div className='message-animate'> 
                                 <h3>Congratulations Explorer!</h3>
                                 <p>If you're reading this that means you've discovered and solved my mini ARG puzzle in my application.</p>
-                                <p>Now that I've got your attention, I thought I would take this opportunity to write out something a little more sincere than I would normally get to put in an application.</p>
-                                <p>Outer Wilds has truly had a profound impact on my life. The game has pushed me to continue to be curious, follow my passions, and helped me connection with so many people.</p>
-                                <p>If you've read this far, I've included a bit of fan-art I made back in 2023 below.</p>
-                                <p> Thanks for bringing a bit more curiosity to the world :)</p>
+                                <p>Now that I've got your attention, I thought I would take this opportunity to write out something a little more sincere than I would normally get to put in an application. Outer Wilds has truly had a profound impact on my life. The game has pushed me to continue to be curious, follow my passions, and helped me connection with so many people. I hope that one day I'll be able to contribute to a project that can impact people the same way Outer Wilds has. If you've read this far, I've included a bit of fan-art I made back in 2023 below.</p>
+                                <p>Thanks for bringing a bit more curiosity to the world :)</p>
                             </div>
                         </div>
 
