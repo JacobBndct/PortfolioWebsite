f534866de01d5a85cf6fa18056ce694276114845
Jacob Benedict <JacobBndct@gmail.com>
2025-04-27 21:01:59 -0400

    new build

--- client/src/components/mobius.component.js
+++ client/src/components/mobius.component.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 
 import '../CSS/gallery.css';
 
+function handleSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+}
+
 export default class Mobius extends Component {
-    handleSubmit = (e) => {
-        e.preventDefault();
-    
-        const form = e.target;
-        const formData = new FormData(form);
-    
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
-      }
+
 
     render() {
         return (
@@ -21,9 +23,12 @@ export default class Mobius extends Component {
                     <div className='normal-section'>
                         <p>Note to self: don't forget to store code someplace safe!</p>
                         <p>Note to note to self: I've put the code </p>
-                        <label>
-                            Code: <input name='code'></input>
-                        </label>
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                Code: <input name='code'></input>
+                            </label>
+                            <button type="submit">Enter</button>
+                        </form>
                     </div>
                 </div>
             </div>
