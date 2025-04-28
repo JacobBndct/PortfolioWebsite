518268c5eb1524d3bede1a2d28ee8642c9d6baf9
Jacob Benedict <JacobBndct@gmail.com>
2025-04-28 03:53:07 -0400

    new build

--- client/src/components/mobius.component.js
+++ client/src/components/mobius.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import '../CSS/gallery.css';
+import '../CSS/mobius.css';
 
 export default class Mobius extends Component {
     constructor(props) {
@@ -47,13 +48,18 @@ export default class Mobius extends Component {
                     </div>
                     {
                         this.state.code !== '' && this.state.code == '--|-..|-.' &&
-                        <div className='message-animate'>
-                            <h3>Congratulations Explorer!</h3>
-                            <p>If you're reading this that means you've discovered and solved my mini ARG puzzle in my application.</p>
-                            <p>Now that I've got your attention, I thought I would take this opportunity to write out something a little more sincere than I would normally get to put in an application.</p>
-                            <p>Outer Wilds has truly had a profound impact on my life. The game has pushed me to continue to be curious, follow my passions, and helped me connection with so many people.</p>
-                            <p>If you've read this far, I've included a bit of fan-art I made back in 2023. Thanks for bringing a bit more curiosity to the world :)</p>
-                            <img src='https://jb-portfolio-website.s3.ca-central-1.amazonaws.com/OuterWilds.png'/> 
+                        <div>
+                            <div className='normal-section message-animate'> 
+                                <h3>Congratulations Explorer!</h3>
+                                <p>If you're reading this that means you've discovered and solved my mini ARG puzzle in my application.</p>
+                                <p>Now that I've got your attention, I thought I would take this opportunity to write out something a little more sincere than I would normally get to put in an application.</p>
+                                <p>Outer Wilds has truly had a profound impact on my life. The game has pushed me to continue to be curious, follow my passions, and helped me connection with so many people.</p>
+                                <p>If you've read this far, I've included a bit of fan-art I made back in 2023 below.</p>
+                                <p> Thanks for bringing a bit more curiosity to the world :)</p>
+                            </div>
+                            <div className='message-animate'>
+                                <img src='https://jb-portfolio-website.s3.ca-central-1.amazonaws.com/OuterWilds.png'/> 
+                            </div>
                         </div>
                        
                     }
