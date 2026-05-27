3d260543f431eff05ad3c4a59e37fbfd253465e9
JacobBndct <66215501+JacobBndct@users.noreply.github.com>
2026-05-27 02:12:16 -0400

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
@@ -11,7 +12,7 @@ const nextConfig: NextConfig = {
     ],
   },
   turbopack: {
-    root: "C:\\Users\\jacob\\Documents\\GitHub\\PortfolioWebsite",
+    root: path.resolve(__dirname),
   },
 };
 
